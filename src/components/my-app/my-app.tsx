42f34341c6f635de94dbdd466b3cc09037d49f61
Esteban Morales <BanditoMac@Estebans-Mini.mhs>
2018-04-30 22:19:57 -0400

    0.0.5

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -1,27 +1,25 @@
-import {Component} from '@stencil/core';
+import { Component } from '@stencil/core';
 
-@Component({tag: 'my-app', styleUrl: 'my-app.css'})
+@Component({ tag: 'my-app', styleUrl: 'my-app.css' })
 export class MyApp {
 
   render() {
     return (
       <div>
         <header class="navbar">
-        <stencil-route-link url="/">
-          <section class="navbar-section">
-            <p class="navbar-brand mr-2">mi icono</p>
-          </section>
+          <stencil-route-link url="/">
+            <section class="navbar-section">
+              <p class="navbar-brand mr-2">mi icono</p>
+            </section>
           </stencil-route-link>
           <section class="navbar-section">
-            <a href="https://github.com/Bandito11" class="btn btn-link">
+            <a href="https://github.com/Bandito11" class="btn btn-link" target="_blank">
               <i class="ionicons ion-social-github"></i>
             </a>
-            <a
-              href="https://www.linkedin.com/in/esteban-morales-5854b67a/"
-              class="btn btn-link">
+            <a href="https://www.linkedin.com/in/esteban-morales-5854b67a/" class="btn btn-link" target="_blank">
               <i class="ionicons ion-social-linkedin"></i>
             </a>
-            <a href="https://twitter.com/Esteban_PR_11" class="btn btn-link">
+            <a href="https://twitter.com/Esteban_PR_11" class="btn btn-link" target="_blank">
               <i class="ionicons ion-social-twitter"></i>
             </a>
           </section>
