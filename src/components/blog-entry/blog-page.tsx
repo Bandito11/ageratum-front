cfae99a802ef7fe54b1564fb6b851afd946e11df
Esteban Morales <red_bandit_11@hotmail.com>
2018-06-27 15:42:45 -0400

    added [ ] to render and erased the gethtml()

--- src/components/blog-entry/blog-page.tsx
+++ src/components/blog-entry/blog-page.tsx
@@ -31,9 +31,7 @@ export class BlogEntry {
     }
     this.content = "";
     const today = new Date();
-    this.date = `${WEEKDAYS[today.getDay()]} ${today.getDate()} de ${
-      MONTHS[today.getMonth()]
-    } de ${today.getFullYear()}`;
+    this.date = `${WEEKDAYS[today.getDay()]} ${today.getDate()} de ${MONTHS[today.getMonth()]} de ${today.getFullYear()}`;
   }
 
   @State() private title;
@@ -76,71 +74,29 @@ export class BlogEntry {
             <label class="form-label" htmlFor="title">
               Title
             </label>
-            <input
-              class="form-input"
-              type="text"
-              id="title"
-              value={this.title}
-              onInput={event => this.getValue({ type: "title", event: event })}
-            />
-
+            <input class="form-input" type="text" id="title" value={this.title} onInput={event => this.getValue({ type: "title", event: event })} />
             <label class="form-label" htmlFor="date">
               Date
             </label>
-            <input
-              class="form-input"
-              type="text"
-              id="date"
-              value={this.date}
-              onInput={event => this.getValue({ type: "date", event: event })}
-            />
-
+            <input class="form-input" type="text" id="date" value={this.date} onInput={event => this.getValue({ type: "date", event: event })} />
             <label class="form-label" htmlFor="headerSRC">
               Header SRC
             </label>
-            <input
-              class="form-input"
-              type="text"
-              id="headerSRC"
-              value={this.headerSrc}
-              onInput={event =>
-                this.getValue({ type: "headerSrc", event: event })
-              }
-            />
-
+            <input class="form-input" type="text" id="headerSRC" value={this.headerSrc} onInput={event => this.getValue({ type: "headerSrc", event: event })} />
             <label class="form-label" htmlFor="headerALT">
               Header ALT
             </label>
-            <input
-              class="form-input"
-              type="text"
-              id="headerALT"
-              value={this.headerAlt}
-              onInput={event =>
-                this.getValue({ type: "headerAlt", event: event })
-              }
-            />
+            <input class="form-input" type="text" id="headerALT" value={this.headerAlt} onInput={event => this.getValue({ type: "headerAlt", event: event })} />
             <hr />
-            <textarea
-              id="textarea"
-              class="form-input"
-              value={this.content}
-              onInput={event =>
-                this.getValue({ type: "textarea", event: event })
-              }
-            />
+            <textarea id="textarea" class="form-input" value={this.content} onInput={event => this.getValue({ type: "textarea", event: event })} />
           </div>
         </form>
       </div>,
       <div id="blog-page">
         <div class="columns">
-          <div class="column col-2" />
+          <div class="column col-2" ></div>
           <div class="column col-8">
-            <img
-              src={this.headerSrc}
-              class="img-responsive"
-              alt={this.headerAlt}
-            />
+            <img src={this.headerSrc} class="img-responsive" alt={this.headerAlt} />
             <h2 id="titleBlog">{this.title}</h2>
             <p>Escrito por: Esteban A. Morales</p>
             <div class="divider" />
@@ -154,12 +110,7 @@ export class BlogEntry {
                   if (contents[index + 1] == "p") {
                     message = "";
                     for (let i = index + 3; i < contents.length; i++) {
-                      if (
-                        contents[i] == "<" &&
-                        contents[i + 1] == "/" &&
-                        contents[i + 2] == "p" &&
-                        contents[i + 3] == ">"
-                      ) {
+                      if (contents[i] == "<" && contents[i + 1] == "/" && contents[i + 2] == "p" && contents[i + 3] == ">") {
                         break;
                       }
                       message += contents[i];
@@ -170,20 +121,11 @@ export class BlogEntry {
                     href = "";
                     message = "";
                     for (let i = index + 3; i < contents.length; i++) {
-                      if (
-                        contents[i] == "<" &&
-                        contents[i + 1] == "/" &&
-                        contents[i + 2] == "a" &&
-                        contents[i + 3] == ">"
-                      ) {
+                      if (contents[i] == "<" && contents[i + 1] == "/" && contents[i + 2] == "a" && contents[i + 3] == ">") {
                         break;
                       }
                       if (
-                        contents[i] == "h" &&
-                        contents[i + 1] == "r" &&
-                        contents[i + 2] == "e" &&
-                        contents[i + 3] == "f"
-                      ) {
+                        contents[i] == "h" && contents[i + 1] == "r" && contents[i + 2] == "e" && contents[i + 3] == "f") {
                         for (let j = i + 6; j < contents.length; j++) {
                           if (contents[j] == '"') {
                             break;
@@ -193,34 +135,16 @@ export class BlogEntry {
                       }
                       if (contents[i] == ">") {
                         for (let j = i + 1; j < contents.length; j++) {
-                          if (
-                            contents[j] == "<" &&
-                            contents[j + 1] == "/" &&
-                            contents[j + 2] == "a" &&
-                            contents[j + 3] == ">"
-                          ) {
+                          if (contents[j] == "<" && contents[j + 1] == "/" && contents[j + 2] == "a" && contents[j + 3] == ">") {
                             break;
                           }
-                          if (
-                            contents[j] == "<" &&
-                            contents[j + 1] == "i" &&
-                            contents[j + 2] == "m" &&
-                            contents[j + 3] == "g"
-                          ) {
+                          if (contents[j] == "<" && contents[j + 1] == "i" && contents[j + 2] == "m" && contents[j + 3] == "g") {
                             src = "";
                             for (let k = j + 5; k < contents.length; k++) {
-                              if (
-                                contents[k] == "/" &&
-                                contents[k + 1] == ">"
-                              ) {
+                              if (contents[k] == "/" && contents[k + 1] == ">") {
                                 break;
                               }
-                              if (
-                                contents[k] == "<" &&
-                                contents[k + 1] == "s" &&
-                                contents[k + 2] == "r" &&
-                                contents[k + 3] == "c"
-                              ) {
+                              if (contents[k] == "<" && contents[k + 1] == "s" && contents[k + 2] == "r" && contents[k + 3] == "c") {
                                 src = "";
                                 for (let m = k + 6; m < contents.length; m++) {
                                   if (contents[m] == '"') {
@@ -229,12 +153,7 @@ export class BlogEntry {
                                   src += contents[m];
                                 }
                               }
-                              if (
-                                contents[k] == "<" &&
-                                contents[k + 1] == "a" &&
-                                contents[k + 2] == "l" &&
-                                contents[k + 3] == "t"
-                              ) {
+                              if (contents[k] == "<" && contents[k + 1] == "a" && contents[k + 2] == "l" && contents[k + 3] == "t") {
                                 alt = "";
                                 for (let m = k + 6; m < contents.length; m++) {
                                   if (contents[m] == '"') {
