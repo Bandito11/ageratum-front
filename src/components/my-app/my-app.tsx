d4930daa6979186a2d6aaf4f41eb47214e960413
Esteban Morales <red_bandit_11@hotmail.com>
2018-06-27 13:38:55 -0400

    changes to blog-page router link

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -28,7 +28,7 @@ export class MyApp {
         <main>
           <stencil-router>
             <stencil-route url='/' exact={true} component='table-of-contents'></stencil-route>
-            <stencil-route url='/blogid/:blogid/title/:title/headersrc/:headersrc/headeralt/:headeralt/date/:date' component='blog-page'></stencil-route>
+            <stencil-route url='/blogid/:blogid' component='blog-page'></stencil-route>
             <stencil-route url='/entry'  component='blog-entry'></stencil-route>
           </stencil-router>
         </main>
