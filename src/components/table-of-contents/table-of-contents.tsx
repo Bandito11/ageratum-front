f698545fbc8d4a9319049061bb9b354bbc67235d
Esteban Morales <red_bandit_11@hotmail.com>
2018-06-27 15:36:51 -0400

    small changes

--- src/components/table-of-contents/table-of-contents.tsx
+++ src/components/table-of-contents/table-of-contents.tsx
@@ -1,7 +1,7 @@
-import { Component, State, Prop } from '@stencil/core';
-import { RouterHistory } from '@stencil/router';
+import { Component, State, Prop } from "@stencil/core";
+import { RouterHistory } from "@stencil/router";
 
-@Component({ tag: 'table-of-contents', styleUrl: 'table-of-contents.css' })
+@Component({ tag: "table-of-contents", styleUrl: "table-of-contents.css" })
 export class TableOfContents {
   @Prop() history: RouterHistory;
   @State() listOfArticles: any[];
@@ -20,69 +20,70 @@ export class TableOfContents {
   }
 
   getListOfArticles() {
-    const url = '/bloglist';
+    const url = "/bloglist";
     return fetch(url);
   }
 
   getHeaderSrc(headerSrc: string) {
-    let src = headerSrc.split('').map(value => {
-      if (value == '/') {
-        return '}';
+    let src = headerSrc.split("").map(value => {
+      if (value == "/") {
+        return "}";
       }
-      if (value == '?') {
-        return '{';
+      if (value == "?") {
+        return "{";
       }
       return value;
-    }).join('');
+    }).join("");
     return src;
   }
 
-  getTitle(title: string) {
-    let src = title.split('').map(value => {
-      if (value == '/') {
-        return '}';
-      }
-      if (value == '?') {
-        return '{';
-      }
-      return value;
-    }).join('');
-    return src;
+  createTitleURL(title: string) {
+    let titleURL = title
+      .trim().split("").map(value => {
+        if (value == " ") {
+          return "-";
+        }
+        if (value == "/") {
+          return "}";
+        }
+        if (value == "?") {
+          return "{";
+        }
+        return value;
+      }).join("");
+    return titleURL;
   }
-
   render() {
     return (
       <div class="container">
         <div class="columns">
-          <div class="column col-1"></div>
+          <div class="column col-1" />
           <div id="table-of-contents" class="column col-10">
             <div class="columns">
-              {this.listOfArticles.map(article =>
+              {this.listOfArticles.map(article => (
                 <div class="column col-xs-12 col-md-6 col-4">
-                  <stencil-route-link url={`/blogid/${article.blogid}/title/${article.title}`}>
+                  <stencil-route-link url={`/blogid/${article.blogid}/title/${this.createTitleURL(article.title)}`}                  >
                     <div class="card">
                       <div class="card-image">
                         <img src={article.headersrc} class="img-responsive" alt={`imagen de ${article.headeralt}`} />
                       </div>
                       <div class="card-header">
-                        <div class="card-title">
-                          {article.title}
-                        </div>
+                        <div class="card-title">{article.title}</div>
                         <div class="card-subtitle text-gray">
                           {article.date}
                         </div>
                       </div>
                       <div class="card-body">
                         {article.content}
-                  </div>
+                      </div>
                     </div>
                     <br />
                   </stencil-route-link>
                 </div>
-              )}
+              ))}
             </div>
           </div>
-          <div class="column col-1"></div>
+          <div class="column col-1" />
         </div>
       </div>
     );
