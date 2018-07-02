268a5ef83cca5167396912755fa557366fae0abf
Esteban Morales <red_bandit_11@hotmail.com>
2018-07-02 12:32:12 -0400

    .

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,3 @@
-
 export interface IBlog {
   id: string;
   title: string;
@@ -8,57 +7,56 @@ export interface IBlog {
   contents: string;
 }
 
-export interface IResponse<T> { 
-    success: boolean;
-    error: any;
-    data: T;
-    dateStamp: Date;
+export interface IResponse<T> {
+  success: boolean;
+  error: any;
+  data: T;
+  dateStamp: Date;
 }
 
-    // enum WEEKDAYS {
-    //   domingo,
-    //   lunes,
-    //   martes,
-    //   miércoles,
-    //   jueves,
-    //   viernes,
-    //   sábado
-    // }
-    // enum MONTHS {
-    //   enero,
-    //   febrero,
-    //   marzo,
-    //   abril,
-    //   mayo,
-    //   junio,
-    //   julio,
-    //   agosto,
-    //   septiembre,
-    //   octubre,
-    //   noviembre,
-    //   diciembre
-    // }
-    // enum WEEKDAYS {
-    //   sunday,
-    //   monday,
-    //   tuesday,
-    //   wednesday,
-    //   thursday,
-    //   friday,
-    //   saturday
-    // }
-    // enum MONTHS {
-    //     january,
-    //     february,
-    //     march,
-    //     april,
-    //     may,
-    //     june,
-    //     july,
-    //     august,
-    //     september,
-    //     october,
-    //     november,
-    //     december
-    //   }
-    
+export enum WEEKDAYS {
+  domingo,
+  lunes,
+  martes,
+  miércoles,
+  jueves,
+  viernes,
+  sábado
+}
+export enum MONTHS {
+  enero,
+  febrero,
+  marzo,
+  abril,
+  mayo,
+  junio,
+  julio,
+  agosto,
+  septiembre,
+  octubre,
+  noviembre,
+  diciembre
+}
+// enum WEEKDAYS {
+//   sunday,
+//   monday,
+//   tuesday,
+//   wednesday,
+//   thursday,
+//   friday,
+//   saturday
+// }
+// enum MONTHS {
+//     january,
+//     february,
+//     march,
+//     april,
+//     may,
+//     june,
+//     july,
+//     august,
+//     september,
+//     october,
+//     november,
+//     december
+//   }
