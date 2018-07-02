c48df080884bb329144cbb9914d31a4ff525b99f
Esteban Morales <red_bandit_11@hotmail.com>
2018-07-02 12:29:57 -0400

    added IBlog interface. Deleted unneeded interfaces

--- src/common.ts
+++ src/common.ts
@@ -1,21 +1,11 @@
 
-export interface IBlogPage {
-    id: string;
-    title: string;
-    content: Partial<IBlogContent>[];
-    author: string;
-    email: string;
-    date: string;
-    headerAlt: string;
-    headerSrc: string;
-}
-
-export interface IBlogContent {
-    alt: string;
-    img: string;
-    text: string;
-    type: string;
-    link: string;
+export interface IBlog {
+  id: string;
+  title: string;
+  date: string;
+  headerAlt: string;
+  headerSrc: string;
+  contents: string;
 }
 
 export interface IResponse<T> { 
@@ -23,15 +13,6 @@ export interface IResponse<T> {
     error: any;
     data: T;
     dateStamp: Date;
-}
-
-export interface ITableOfContents{
-    id: string;
-    title: string;
-    content: Partial<IBlogContent>[];
-    date: string;
-    headerAlt: string;
-    headerSrc: string;
 }
 
     // enum WEEKDAYS {
