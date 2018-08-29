2f13105257a93582b75c5730f7fbb54ca1371f6e
Esteban Morales <red_bandit_11@hotmail.com>
2018-08-29 16:07:39 -0400

    preliminary implementation

--- src/providers/ageratum-db.ts
+++ src/providers/ageratum-db.ts
@@ -1,130 +1,152 @@
-// const db = 'TODO: Loki logic goes here';
 
-// export function createDB() { }
+/**
+ * Check if there is data save of local DB
+ * 
+ * If there is data on db, load on the main page
+ * 
+ * If there no data on db, get from API by ids not on db
+ * Insert new Data from API into db
+ * load data from db into main page
+ * 
+ */
+import { AxiosStatic } from '../../axios';
+import { domain } from '../../common';
 
-// export function findOne(id) {
-//     // Will be deleted when Loki is implemented
-//     return true
-// }
-// export function insertArticle(article: IBlog) {
-//     let response: IResponse<string> = {
-//         success: false,
-//         error: null,
-//         data: undefined,
-//         dateStamp: new Date()
-//     }
-//     const found = findOne(article.id); //Will use Loki core Find Method.
-//     if (!found) {
-//         try {
-//             //TODO: 
-//             // Insert article into DB
-//             response = {
-//                 ...response,
-//                 success: true,
-//                 data: 'Article was saved successfully!'
-//             }
-//             return response;
+import * as loki from 'loki';
+declare const myAdapter;
+declare const axios: AxiosStatic;
 
-//         } catch (error) {
-//             // There was an error on LokiJS
-//             response = {
-//                 ...response,
-//                 error: error
-//             }
-//             return response;
-//         }
-//     } else {
-//         response = {
-//             ...response,
-//             error: 'Article was found on Local DB!'
-//         }
-//         return response;
-//     }
-// }
+export interface IBlog {
+    id: string;
+    title: string;
+    date: string;
+    headeralt: string;
+    headersrc: string;
+    contents: string;
+}
 
-// export function updateArticle(article: IBlog) {
-//     let response: IResponse<string> = {
-//         success: false,
-//         error: null,
-//         data: undefined,
-//         dateStamp: new Date()
-//     }
-//     const found = findOne(article.id); //Will use Loki methods to find if Article is on DB
-//     if (found) {
-//         try {
-//             //TODO: 
-//             // Update article into DB
-//             response = {
-//                 ...response,
-//                 success: true,
-//                 data: 'Article was updated successfully!'
-//             }
-//             return response;
+export interface IResponse<T> {
+    success: boolean;
+    error: any;
+    data: T;
+    dateStamp: Date;
+}
 
-//         } catch (error) {
-//             // There was an error on LokiJS
-//             response = {
-//                 ...response,
-//                 error: error
-//             }
-//             return response;
-//         }
-//     } else {
-//         //Implement message explaining to the user that article was not found and will insert in DB instead
-//         insertArticle(article);
-//     }
-// }
+let domain;
+function getListOfArticles() {
+    const url = `${domain}/blog/list`;
+    return axios.get(url);
+    // .then(response => {
+    //     if (response.status == 200) {
+    //       this.blogLoaded = true;
+    //       response.data.success ?
+    //         this.listOfArticles = [...this.listOfArticles, ...response.data.data] :
+    //         alert('There was an error retrieving the data. Please reload the page and try again!');
+    //     }
+    //   });
+}
 
+const db = create();
+let usersColl;
 
-// export function deleteArticle(id: string) {
-//     let response: IResponse<string> = {
-//         success: false,
-//         error: null,
-//         data: undefined,
-//         dateStamp: new Date()
-//     }
-//     const found = findOne(id); //Will use Loki methods to find if Article is on DB by id
-//     if (found) {
-//         try {
-//             //TODO: 
-//             // Delete article from DB
-//             response = {
-//                 ...response,
-//                 success: true,
-//                 data: 'Article was deleted successfully!'
-//             }
-//             return response;
+function create() {
+    const options = {
+        autoloadCallback: loadDatabase,
+        autoload: true,
+        env: 'BROWSER'
+    };
+    return new loki('ageratum.db', options);
+}
 
-//         } catch (error) {
-//             // There was an error on LokiJS
-//             response = {
-//                 ...response,
-//                 error: error
-//             }
-//             return response;
-//         }
-//     } else {
-//         //Implement message explaining to the user that article was not found and will insert in DB instead
-//         response = {
-//             ...response,
-//             error: 'Article was not found on Local DB!'
-//         }
-//         return response;
-//     }
-// }
+function loadDatabase() {
+    usersColl = db.getCollection('Users');
+    if (!usersColl) {
+        usersColl = db.addCollection('Users');
+    }
+}
 
-// export interface IBlog {
-//     id: string;
-//     title: string;
-//     date: string;
-//     headeralt: string;
-//     headersrc: string;
-//     contents: string;
-// }
+function insert(page) {
+    let response: IResponse<null> = {
+        success: false,
+        error: null,
+        data: null,
+        dateStamp: new Date()
+    };
+    try {
+        usersColl.insert(page);
+        db.saveDatabase();
+        response = {
+            ...response,
+            success: true
+        };
+        return response;
+    } catch (error) {
+        response = {
+            ...response,
+            success: false,
+            error: error
+        };
+        return response;
+    }
+}
 
-// export interface IResponse<T> {
-//     success: boolean;
-//     error: any;
-//     data: T;
-//     dateStamp: Date;
-// }
+function update(page) {
+    let response: IResponse<null> = {
+        success: false,
+        error: null,
+        data: null,
+        dateStamp: new Date()
+    };
+    try {
+        let result = usersColl.findOne({ name: { $eq: page.id } });
+        result = { ...page };
+        usersColl.update(result);
+        db.saveDatabase();
+        response = {
+            ...response,
+            success: true
+        };
+        return response;
+    } catch (error) {
+        response = {
+            ...response,
+            success: false,
+            error: error
+        };
+        return response;
+    }
+}
+
+function remove(id) {
+    let response: IResponse<null> = {
+        success: false,
+        error: null,
+        data: null,
+        dateStamp: new Date()
+    };
+    try {
+        const result = usersColl.findOne({ name: { $eq: id } });
+        usersColl.remove(result);
+        db.saveDatabase();
+        response = {
+            ...response,
+            success: true
+        };
+        return response;
+    } catch (error) {
+        response = {
+            ...response,
+            success: false,
+            error: error
+        };
+        return response;
+    }
+}
+
+function getArticles() {
+    return usersColl.data;
+}
+
+function getArticle(id) {
+    return usersColl.findOne({ name: { $eq: id } });
+}
